aab7c321eaa71a28d9be9cff6d6bc0d806b73fff
Vita <vitac215@gmail.com>
2017-04-15 11:48:10 -0400

    implement drag and drop

--- js/visualEditor.js
+++ js/visualEditor.js
@@ -1,4 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
+/*
+  Ondrag handler
+  Save the source object's id
+*/
+function drag(event) {
+  event.dataTransfer.setData("id", event.target.id);
+}
 
+/*
+  Ondragover handler
+*/
+function dragover(event) {
+  event.preventDefault();
+}
 
-});
+/*
+  Ondrop handler
+*/
+function drop(event) {
+  event.preventDefault();
+
+  // get the container, get source id and source img
+  let container = event.target;
+  let source_id = event.dataTransfer.getData("id");
+  let source_img = document.getElementById(source_id);
+
+  // when drops, append the img to the container
+  if (source_img.width !== container.offsetWidth || source_img.height !== container.offsetHeight ) {
+    source_img.width = container.offsetWidth * 0.9;
+    source_img.height = container.offsetHeight * 0.9;
+  }
+  container.appendChild(source_img);
+}
